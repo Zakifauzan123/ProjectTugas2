d5be34c6cffa017138ab5f2415d38de8dbf51008
Zakifauzan <202210715297@mhs.ubhrajaya.ac.id>
2022-10-29 16:04:56 +0700

    tugas2 sudah selesai
    kirim

---
+++ pernyataan 4/pernyataan4.py
@@ -0,0 +1,18 @@
+# pengulangan pakai for
+for i in range(8):
+    print("pengulangan ke -", i)
+
+print("\n")
+
+listMinuman = ["Air Putih", "Kopi", "Teh", "Jamu", "Whisky"]
+for Minuman in listMinuman :
+    print(Minuman)
+
+print("\n")
+
+# pengunglangan pakai while
+angka = 2
+
+while angka > 10 :
+    angka += 1
+    print("contoh penggunaan sintax while")
---
+++ pernyataan2/pernyataan2.py
@@ -0,0 +1,6 @@
+# pencabangan If dengan 1 kasus
+nama =input ("Masukan Nama : ")
+if nama == "Zaki Nur Fauzan" :
+  print("Hai", nama, "selamat datang")
+  print()
+  
